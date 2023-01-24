7298245b2d9c77f339c9694350ed5ccb774359c7
nelsondev19 <nelsonher019@gmail.com>
2023-01-23 21:37:22 -0600

    [FIX] Navbar design

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,34 +6,66 @@ function Navbar() {
     <>
       <nav className="block md:hidden m-3">
         <div className="grid grid-cols-2">
-          <div className="font-extrabold">
-            DenoLyrics
-          </div>
+          <div className="font-extrabold">DenoLyrics</div>
           <div className="flex justify-end">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
             </svg>
           </div>
         </div>
       </nav>
-      <nav className="hidden md:block m-5 font-semibold text-[#6D7073]">
+      <nav className="hidden md:block font-semibold text-[#6D7073] backdrop-blur-sm bg-white/80 sticky top-0 z-50 p-2">
         <div className="grid grid-cols-2">
-          <div>
-            <Link href="/" className="select-none px-5 py-2.5 border border-transparent font-bold">DenoLyrics</Link>
+          <div className="flex justify-start items-start">
+            <Link
+              href="/"
+              className="select-none px-5 py-2.5 font-bold border border-transparent"
+            >
+              DenoLyrics
+            </Link>
           </div>
           <div className="flex justify-end items-end gap-3">
             <div className="flex justify-center items-center">
-              <Link href="/" className="select-none px-5 py-2.5 border border-transparent">Home</Link>
+              <Link
+                href="/"
+                className="select-none px-5 py-2.5 border border-transparent"
+              >
+                Home
+              </Link>
             </div>
             <div className="flex justify-center items-center">
-              <Link href="/about" className="select-none px-5 py-2.5 border border-transparent">Pricing</Link>
+              <Link
+                href="/about"
+                className="select-none px-5 py-2.5 border border-transparent"
+              >
+                Pricing
+              </Link>
             </div>
             <div className="flex justify-center items-center">
-              <Link href="/about" className="select-none px-5 py-2.5 border border-transparent">Help</Link>
+              <Link
+                href="/about"
+                className="select-none px-5 py-2.5 border border-transparent"
+              >
+                Help
+              </Link>
             </div>
 
             <div className="flex justify-center items-center">
-              <Link href={"/"} className="select-none text-white hover:shadow-md bg-[#363141] hover:bg-black focus:outline-none font-medium rounded-lg px-5 py-2.5 border border-transparent">
+              <Link
+                href={"/"}
+                className="select-none text-white hover:shadow-md bg-[#363141] hover:bg-black focus:outline-none font-medium rounded-lg px-5 py-2.5 border border-transparent"
+              >
                 Get started for free
               </Link>{" "}
             </div>
