b755f887dc98b809a9195946ea3dac62810fa23d
nelsondev19 <nelsonher019@gmail.com>
2023-01-24 21:58:02 -0600

    add changes

---
+++ app/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <p className="text-center mt-5">{new Date().getFullYear()} DenoLyrics.</p>
+      <br />
+    </>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // STYLES
 import "../styles/globals.css";
 import "../styles/font.css";
-
+import Footer from "./footer";
 
 export default function RootLayout({
   children,
@@ -15,7 +15,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
