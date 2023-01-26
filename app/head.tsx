ec300e72465c4025e7167ba187bbabacc3bb4e05
nelsondev19 <nelsonher019@gmail.com>
2023-01-25 23:21:12 -0600

    add preview image

--- app/head.tsx
+++ app/head.tsx
@@ -8,6 +8,7 @@ export default function Head() {
         content="Get text to audio with few clicks through Artificial Intelligence, DenoLyrics is a web application that understands many languages."
       />
       <link rel="icon" href="/denolyrics.svg" type="image/svg" />
+      <meta property="og:image" content="/preview.png" />
     </>
   );
 }
