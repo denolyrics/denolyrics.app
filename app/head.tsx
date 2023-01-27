756afc54ad507bd8161b3f795fbf463dd37eba2e
nelsondev19 <nelsonher019@gmail.com>
2023-01-26 22:07:36 -0600

    add metadata

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,33 @@
 export default function Head() {
+  // @ts-ignore
+  const DOMAIN =
+    process.env.NODE_ENV === "production"
+      ? "https://www.denolyrics.com"
+      : "http://localhost:3000";
+
+  const description =
+    "Get text to audio with few clicks through Artificial Intelligence, DenoLyrics is a web application that understands many languages.";
   return (
     <>
       <title>DenoLyrics</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content={description} />
+      <meta name="theme-color" content="#292936" />
+      <meta property="og:title" content="DenoLyrics" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={DOMAIN} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={DOMAIN} />
+      <meta name="twitter:creator" content="@denolyrics" />
+      <meta name="twitter:title" content="DenoLyrics" />
+      <meta property="twitter:description" content={description} />
+
       <meta
-        name="description"
-        content="Get text to audio with few clicks through Artificial Intelligence, DenoLyrics is a web application that understands many languages."
+        name="keywords"
+        content="DenoLyrics, Audio to Text with AI, El Salvador, Get text to audio with few clicks through Artificial Intelligence,web application that understands many languages."
       />
+
       <link rel="icon" href="/denolyrics.svg" type="image/svg" />
       <meta property="og:image" content="/preview.png" />
     </>
