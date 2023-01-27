f28b63391962a506707934893f9c9b60193332e8
nelsondev19 <nelsonher019@gmail.com>
2023-01-26 21:42:27 -0600

    [ADD] steps for using the app

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main>
       <div className="mt-20 md:mt-10">
         <div className="flex justify-center items-center">
-        <svg
+          <svg
             width={182}
             height={194}
             viewBox="0 0 182 194"
@@ -358,7 +358,7 @@ export default function Home() {
       <br />
       <Faq />
 
-      <div className="border-t border-slate-300 mt-10 md:mt-40 pt-10 md:pt-20">
+      <div className="border-t border-slate-300 mt-10 md:mt-10 pt-10 md:pt-20">
         <div className="flex justify-center items-center">
           <svg
             width={182}
@@ -624,6 +624,414 @@ export default function Home() {
           >
             Get started for free
           </Link>
+        </div>
+      </div>
+
+      <div className="grid place-content-center mt-10  p-3">
+        <div className="max-w-screen-xl m-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            <div>
+              <svg
+                viewBox="0 0 1920 1080"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="1920" height="1080" fill="#F9F9F8" />
+                <rect width="121" height="1080" fill="#EDEDED" />
+                <rect
+                  x="514"
+                  y="239"
+                  width="933"
+                  height="492"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <path
+                  d="M1137 661C1137 657.686 1139.69 655 1143 655H1254C1257.31 655 1260 657.686 1260 661V696C1260 699.314 1257.31 702 1254 702H1143C1139.69 702 1137 699.314 1137 696V661Z"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="545"
+                  y="485"
+                  width="878"
+                  height="74"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="545"
+                  y="265"
+                  width="352"
+                  height="37"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="639"
+                  y="29"
+                  width="642"
+                  height="37"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="29"
+                  width="180"
+                  height="37"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="545"
+                  y="326"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1300"
+                  y="655"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1774"
+                  y="117"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="1613"
+                  y="117"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <circle cx="1854.5" cy="35.5" r="18.5" fill="#EDEDED" />
+              </svg>
+              <div className="text-2xl md:text-4xl font-bold mt-5">
+                <span>Create a folder</span>
+              </div>
+              <p className="mt-2">Step 1</p>
+              <p className="text-base font-normal mt-2">
+                Create a folder to later upload your audio files.
+              </p>
+            </div>
+            <div>
+              <svg
+                viewBox="0 0 1920 1080"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="1920" height="1080" fill="#F9F9F8" />
+                <rect width="121" height="1080" fill="#EDEDED" />
+                <rect
+                  x="252"
+                  y="199"
+                  width="1535"
+                  height="415"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="252"
+                  y="736"
+                  width="1535"
+                  height="45"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="252"
+                  y="806"
+                  width="1535"
+                  height="45"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="252"
+                  y="666"
+                  width="1535"
+                  height="45"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="1504"
+                  y="903"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="639"
+                  y="29"
+                  width="642"
+                  height="37"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="29"
+                  width="180"
+                  height="37"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="1664"
+                  y="903"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <circle cx="1854.5" cy="35.5" r="18.5" fill="#EDEDED" />
+              </svg>
+
+              <div className="text-2xl md:text-4xl font-bold mt-5">
+                <span>Upload Audio Files</span>
+              </div>
+              <p className="mt-2">Step 2</p>
+              <p className="text-base font-normal mt-2">
+                Select your audio files with mp3 format.
+              </p>
+            </div>
+
+            <div>
+              <svg
+                viewBox="0 0 1920 1080"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="1920" height="1080" fill="#F9F9F8" />
+                <rect width="121" height="1080" fill="#EDEDED" />
+                <path
+                  d="M1018 201C1018 197.686 1020.69 195 1024 195H1870C1873.31 195 1876 197.686 1876 201V973C1876 976.314 1873.31 979 1870 979H1024C1020.69 979 1018 976.314 1018 973V201Z"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="203"
+                  width="831"
+                  height="127"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="402"
+                  width="831"
+                  height="127"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="601"
+                  width="831"
+                  height="127"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="779"
+                  width="831"
+                  height="127"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="639"
+                  y="29"
+                  width="642"
+                  height="37"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="162"
+                  y="29"
+                  width="180"
+                  height="37"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="1753"
+                  y="117"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="870"
+                  y="117"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <rect
+                  x="709"
+                  y="117"
+                  width="123"
+                  height="47"
+                  rx="6"
+                  fill="#EDEDED"
+                />
+                <circle cx="1854.5" cy="35.5" r="18.5" fill="#EDEDED" />
+                <rect
+                  x="1085"
+                  y="267"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="393"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="561"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="603"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="771"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="435"
+                  width="730"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="351"
+                  width="591"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="813"
+                  width="591"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="855"
+                  width="591"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="687"
+                  width="591"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="519"
+                  width="591"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="309"
+                  width="408"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="477"
+                  width="408"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="645"
+                  width="408"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="729"
+                  width="408"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+                <rect
+                  x="1085"
+                  y="897"
+                  width="408"
+                  height="21"
+                  rx="6"
+                  fill="#E0E0E0"
+                />
+              </svg>
+
+              <div className="text-2xl md:text-4xl font-bold mt-5">
+                <span>Processing Text</span>
+              </div>
+              <p className="mt-2">Step 3</p>
+              <p className="text-base font-normal mt-2">
+                Processing audio on the server to get the text.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </main>
