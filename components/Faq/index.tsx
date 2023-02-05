b4bd7c291139f125fc7d4a3aeaf58666e95a1f09
Nelson Hernández <50471203+nelsondev19@users.noreply.github.com>
2023-02-05 14:13:59 -0600

    Update index.tsx

--- components/Faq/index.tsx
+++ components/Faq/index.tsx
@@ -50,7 +50,7 @@ const faqs = [
   {
     id: 2,
     question: "What data does DenoLyrics send to the cloud?",
-    answer: `Denolyrics Whisper is a general-purpose speech recognition model. It is trained on a large dataset of diverse audio and is also a multi-task model that can perform multilingual speech recognition as well 
+    answer: `Whisper is a general-purpose speech recognition model. It is trained on a large dataset of diverse audio and is also a multi-task model that can perform multilingual speech recognition as well 
     as speech translation and language identification.`,
   },
   {
