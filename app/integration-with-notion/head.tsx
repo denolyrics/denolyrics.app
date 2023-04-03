81588b116a19a49c99c4dc8d8176220748f759db
nelsondev19 <nelsonher019@gmail.com>
2023-04-02 21:44:38 -0600

    add changes

--- app/integration-with-notion/head.tsx
+++ app/integration-with-notion/head.tsx
@@ -13,31 +13,31 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={description} />
       <meta name="theme-color" content="#180821" />
-      <meta property="og:title" content="DenoLyrics | Pricing" />
+      <meta property="og:title" content="Notion integration" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={DOMAIN} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={DOMAIN} />
       <meta name="twitter:creator" content="@denolyrics" />
-      <meta name="twitter:title" content="DenoLyrics | Pricing" />
+      <meta name="twitter:title" content="Notion integration" />
       <meta property="twitter:description" content={description} />
       <meta
         property="twitter:image"
-        content="https://www.denolyrics.com/preview.png"
+        content="https://www.denolyrics.com/notion.png"
       />
       <meta name="robots" content="all" />
 
       <meta
         name="keywords"
-        content="web application, Audio to Text with AI, Integration with Notion"
+        content="web application, Audio to Text with AI, Integration with Notion, Notion pages"
       />
       <meta property="og:image:width" content="1920" />
       <meta property="og:image:height" content="1080" />
       <link rel="icon" href="/denolyrics.svg" type="image/svg" />
       <meta
         property="og:image"
-        content="https://www.denolyrics.com/preview.png"
+        content="https://www.denolyrics.com/notion.png"
       />
     </>
   );
